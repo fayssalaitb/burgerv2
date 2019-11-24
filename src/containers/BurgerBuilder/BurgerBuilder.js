6e91d9edd6e84beb8e9f1b997dd07758980fefba
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-11-24 17:50:09 +0100

    worked on the layout and burger burgeringredient and CSS files the burger is showing in the page

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/aux";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuilder extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Aux>
+        <Burger />
+        <div>build control</div>
+      </Aux>
+    );
+  }
+}
+export default BurgerBuilder;
