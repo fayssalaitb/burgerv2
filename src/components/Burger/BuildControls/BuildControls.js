7545fc00589153be9cf93ebc62ce54135e358b65
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-11-28 14:55:23 +0100

    add a Order button and the logic . the updatePurchasestate method had to to recieve the ingredients and not fitch them directly due to the setstate work

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -27,6 +27,9 @@ const BuildControls = props => {
           />
         );
       })}
+      <button className={classes.OrderButton} disabled={!props.purchasable}>
+        ORDER NOW
+      </button>
     </div>
   );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,22 @@ class BurgerBuilder extends Component {
       cheese: 0
     },
 
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  };
+
+  updatePurchaseState = ingredients => {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+
+    this.setState({ purchasable: sum > 0 });
+    console.log(sum);
+    console.log(this.state.purchasable);
   };
 
   addIngredientHandler = type => {
@@ -34,6 +49,7 @@ class BurgerBuilder extends Component {
     const newPrice = oldPrice + INGREDIENT_PRICE[type];
 
     this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   removeIngredientHandler = type => {
@@ -50,6 +66,7 @@ class BurgerBuilder extends Component {
     const newPrice = oldPrice - INGREDIENT_PRICE[type];
 
     this.setState({ ingredients: updatedIngredients, totalPrice: newPrice });
+    this.updatePurchaseState(updatedIngredients);
   };
 
   render() {
@@ -65,6 +82,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
         />
       </Aux>
     );
