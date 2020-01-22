d5f136befb570229c90364d03e0e178b126ec2dd
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2020-01-22 15:42:57 +0100

    pass some data fron BurgerBuilder.js to Chekout.js using routes history.push

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,29 @@ class Checkout extends Component {
       bacon: 1
     }
   };
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
+  }
+
+  CheckoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+  CheckoutContinuedHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
+  };
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          CheckoutCancelled={this.CheckoutCancelledHandler}
+          CheckoutContinued={this.CheckoutContinuedHandler}
+        />
       </div>
     );
   }
