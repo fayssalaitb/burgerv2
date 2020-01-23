5e657bea2132072f8ec5d8d7f281900960b11958
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2020-01-23 15:34:52 +0100

    Outputing the Orders, i also hage a css  prb in the Orders page that i need to fix

--- src/components/Order/Order/Order.js
+++ src/components/Order/Order/Order.js
@@ -2,11 +2,34 @@ import React from "react";
 import classes from "./Order.module.css";
 
 const Order = props => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
+      <p>Ingredients: {ingredientOutput}</p>
       <p>
-        Price: <strong>USD 4</strong>
+        Price: <strong>USD {props.price}</strong>
       </p>
     </div>
   );
